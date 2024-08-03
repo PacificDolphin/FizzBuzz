48426e0d67530cb41e9d05db20ad4fb6abb4bf87
PacificDolphin <161969244+PacificDolphin@users.noreply.github.com>
2024-08-03 12:11:59 -0600

    Update FizzBuzzInnovation.py
    fizz buzz should be functional now!

--- FizzBuzzInnovation.py
+++ FizzBuzzInnovation.py
@@ -1,9 +1,12 @@
 for x in range(1, 100):
-	if x%3==0:
-		print("fizz, the very innovative feature")
-	elif x%5==0:
-		print("buzz, another feature exemplifying the team's dedication to innovation")
-	elif x%15==0:
-		print("fizz buzz, the perfect combination of both achievment and innovation. a truly outstanding product!")
-	elif x=37:
-		print("i want more infinite descent.")
+  if x%3==0:
+    if x%5==0:
+      print("fizz buzz, an outstanding product, and a noble number")
+    elif x%5 !=0:
+      print("fizz, the very innovative feature")
+  elif x%5==0:
+    print("buzz, another feature exemplifying the team's dedication to innovation")
+  elif x==37:
+    print("such a noble number and a marvelous prime, unparallelled in it's wisdom")
+  elif x==13:
+    print("such a noble nunber and a marvelous prime, deserving the throne")
